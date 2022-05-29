28f168da7835f7ef60c583d96e1c76a605012711
wanghepeng <tanhua2023@163.com>
2022-05-29 22:25:48 +0800

    😎feat: 注册全局icon

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,9 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+svg {
+  width: 1em;
+  height: 1em;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import router from '@/router'
 import UI from '@/components'
+import { toLine } from './utils'
+import './mock'
 
 const app = createApp(App)
-import './mock'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(`el-icon-${toLine(key)}`, component as any)
+}
+
 
 app.use(router).use(ElementPlus).use(UI)
 app.mount('#app')
